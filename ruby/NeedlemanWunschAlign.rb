da9bc2efc3ab249297b201bbd6807684ece64434
KojiKobayashi <k.kobayashi@33i.co.jp>
2019-07-11 18:41:43 +0900

    feat: get multiple results

--- ruby/NeedlemanWunschAlign.rb
+++ ruby/NeedlemanWunschAlign.rb
@@ -59,6 +59,76 @@ class NeedlemanWunschAlign
         return [aling_a.reverse, aling_b.reverse, table[a.length][b.length]]
     end
 
+
+    include Enumerable
+    def initialize(a, b)
+        @a = a
+        @b = b
+    end
+    def each(&block)
+
+        table = Array.new(@a.length + 1).map{ Array.new(@b.length + 1, 0) }
+
+        # set upper row and left coloum
+        table.each_with_index do |row, i|
+            row[0] = -i
+        end
+        table[0].length.times do |i|
+            table[0][i] = -i
+        end
+
+        @a.length.times do |j|
+            @b.length.times do |i|
+
+                score_to_bottom = table[j][i + 1] - 1
+                score_to_right = table[j + 1][i] - 1
+                score_to_diagonal = table[j][i] - 1
+                score_to_diagonal += 2 if @a[j] == @b[i]
+
+                score = [score_to_bottom, score_to_right, score_to_diagonal].max
+                table[j + 1][i + 1] = score
+            end
+        end
+
+        # derive only 1 candidate
+        r = @a.length
+        c = @b.length
+        aling_a = ""
+        aling_b = ""
+
+        iterate(r, c, table, aling_a, aling_b, &block)
+    end
+
+    private
+        def iterate(r, c, table, aling_a, aling_b, &block)
+            if r == 0 and c == 0
+                yield [aling_a.reverse, aling_b.reverse, table[@a.length][@b.length]]
+                return
+            end
+
+            s = @a[r-1] == @b[c-1] ? 1 : -1
+
+            if r > 0 and c > 0 and table[r][c] == table[r-1][c-1] + s
+                iterate(r-1, c-1, table, aling_a+@a[r-1], aling_b+@b[c-1], &block)
+            end
+
+            if r > 0 and table[r][c] == table[r-1][c] - 1
+                # aling_a+=(a[r-1])
+                # aling_b+=("_")
+                # r -= 1
+                # next
+                iterate(r-1, c, table, aling_a+@a[r-1], aling_b+"_", &block)
+            end
+
+            if c > 0 and table[r][c] == table[r][c-1] - 1
+                # aling_b+=(b[c-1])
+                # aling_a+=("_")
+                # c -= 1
+                iterate(r,c-1,table, aling_a+"_", aling_b+@b[c-1], &block)
+            end
+
+            return
+        end
 end
 
 if $0 == __FILE__
--- ruby/test/NeelemanWunschAlignTests.rb
+++ ruby/test/NeelemanWunschAlignTests.rb
@@ -7,4 +7,11 @@ class NeedlemanWunschAlignTest < Test::Unit::TestCase
         assert_equal NeedlemanWunschAlign.align("GATTACA","GCATGCU")[2], 0
         assert_equal NeedlemanWunschAlign.align("abcde","abcde"), ["abcde", "abcde", 5]
     end
+
+    def test_yield
+        arr = NeedlemanWunschAlign.new("GATTACA","GCATGCU").map{|i| i}
+        assert_equal arr[0], ["G_ATTACA", "GCA_TGCU", 0]
+        assert_equal arr[1], ["G_ATTACA", "GCAT_GCU", 0]
+        assert_equal arr[2], ["G_ATTACA", "GCATG_CU", 0]
+    end
 end
